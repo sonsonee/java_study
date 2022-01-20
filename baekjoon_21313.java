8d0df3827a641a89a2a4feb901b752b4a00f6d87
dkdlxpa135@gmail.com <dkdlxpa135@gmail.com>
2022-01-20 21:59:46 +0900

    220120

---
+++ baekjoon_21313.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class baekjoon_21313{
+
+	static Scanner scanner = new Scanner(System.in);
+	static StringTokenizer st;
+	static StringBuilder result = new StringBuilder();
+	
+	public static void main (String[] args){
+		
+		int n = scanner.nextInt();
+		
+		int odd = 0;
+		if(n % 2 == 1) odd = 1;
+
+		for(int i = 0; i < n - odd; i += 2){
+			result.append(1).append(' ').append(2).append(' ');
+		}
+		
+		if(odd == 1) result.append(3);
+		
+		System.out.print(result.toString());
+	}
+}
