857d6f1083151099bd80c3b29b9e0632702fd9bc
dkdlxpa135@gmail.com <dkdlxpa135@gmail.com>
2022-01-25 22:36:53 +0900

    220125

--- baekjoon_15655.java
+++ baekjoon_15655.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-class baekjoon_15649{
+class baekjoon_15655{
 	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -10,6 +10,7 @@ class baekjoon_15649{
 	
 	private static int N, M;
 	private static int[] arr;
+	private static int[] tmp;
 	private static boolean[] visited;
 
 	public static void main(String[] args) throws IOException{
@@ -18,32 +19,40 @@ class baekjoon_15649{
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		
+		visited = new boolean[N];
+		arr = new int[N];
+		tmp = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
 		br.close();
 		
-		arr = new int[N]; // 1 ~ N 값이 들어갈 배열
-		visited = new boolean[N];	// 방문한 곳인지 표시
-
-		dfs(0);
+		Arrays.sort(arr);
+		backtrack(0, 0);
+		
 		bw.write(result.toString());
 		bw.flush();
 		bw.close();
 	}
 	
-	public static void dfs(int depth){
-		
+	public static void backtrack(int depth, int start){
 		if(depth == M){
 			for(int i = 0; i < M; i++){
-				result.append(arr[i]).append(' ');
+				result.append(tmp[i]).append(' ');
 			}
 			result.append('\n');
 		}
 		else{
-			for(int i = 0; i < N; i++){
+			for(int i = start; i < N; i++){
 				if(!visited[i]){
-					visited[i] = true;	// 방문한 곳임을 표시
-					arr[depth] = i + 1; // 배열에 값 저장
-					dfs(depth+1);		// 더 깊이 탐색
-					visited[i] = false;	// 재귀 이후에는 visited 다시 false로 변경
+					visited[i] = true;
+					tmp[depth] = arr[i];
+					backtrack(depth+1, i+1);
+					visited[i] = false;
 				}
 			}
 		}
