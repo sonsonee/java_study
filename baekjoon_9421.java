c8580f7532e2d3c22ab2b0bfbf8625fe5c71a21b
dkdlxpa135@gmail.com <dkdlxpa135@gmail.com>
2022-02-07 00:31:49 +0900

    220207

--- baekjoon_9421.java
+++ baekjoon_9421.java
@@ -1,14 +1,17 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 class baekjoon_9421{
 	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter();
-	static StringBuilder result;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder result = new StringBuilder();
 	
 	static boolean[] d;
-	static boolean[] e;
+	static int[] e;
 	
 	public static void main(String[] args) throws IOException{
 		
@@ -16,7 +19,7 @@ class baekjoon_9421{
 		br.close();
 		
 		d = new boolean[N+1];
-		e = new boolean[N+1];
+		e = new int[N+1];
 		
 		// 에라토스테네스의 체
 		for (int i = 2; i <= N; i++) {
@@ -26,10 +29,15 @@ class baekjoon_9421{
 			}
 		}
 		
-		// 상근수 거르기 : 상근수면 false
+		// 상근수 거르기 : 소수 상근수면 1, 아니면 2
 		for(int i = 2; i <= N; i++){
-			if(e[i]) continue;
-			
+			if(!d[i]) e[i] = 2;
+			if(e[i] == 0)
+				e[i] = recursive(i);
+		}
+		
+		for(int i = 2; i <= N; i++){
+			if(e[i] == 1) result.append(i).append("\n");
 		}
 		
 		bw.write(result.toString());
@@ -37,7 +45,25 @@ class baekjoon_9421{
 		bw.close();
 	}
 	
-	boolean recursive(){
-		
+	static int recursive(int i){
+		if(e[i] == 2){
+			return 2;
+		}
+		else if(i == 0){
+			return 1;
+		}
+		else{
+			String s = Integer.toString(i);
+			char[] digit = s.toCharArray();
+			int sum = 0;
+			for(char c:digit){
+				sum += (int)((c - '0') * (c - '0'));
+			}
+
+			e[i] = recursive(sum);
+			
+			if(e[i] == 1) return 1;
+			else return 2;
+		}
 	}
 }
