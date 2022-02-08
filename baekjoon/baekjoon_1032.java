cebcc70a085aac6ace1e6271e2fa2b414bb928d0
dkdlxpa135@gmail.com <dkdlxpa135@gmail.com>
2022-02-08 22:32:34 +0900

    220208

---
+++ baekjoon/baekjoon_1032.java
@@ -0,0 +1,33 @@
+package baekjoon;
+import java.io.*;
+
+class baekjoon_1032{
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder result;
+    public static void main(String[] args) throws IOException{
+
+        String s = br.readLine();
+        int N = Integer.parseInt(s);
+        String[] arr = new String[N];
+    
+        for(int i = 0; i < N; i++){
+            arr[i] = br.readLine();
+        }
+
+        result = new StringBuilder();
+        for(int i = 0; i < arr[0].length(); i++){
+            boolean check = true;
+            for(int j = 0; j < N - 1; j++){
+                if(arr[j].charAt(i) != arr[j+1].charAt(i)){
+                    check = false;
+                }
+            }
+            
+            if(check) result.append(arr[0].charAt(i));
+            else result.append('?');
+        }
+
+        System.out.print(result.toString());
+    }
+}
